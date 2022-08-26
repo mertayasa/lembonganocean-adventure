1de8aadd3b23393cca1751c05ee4342a3ae28387
mertayasa <made.b4li@gmail.com>
2022-08-27 00:06:22 +0800

    Update meta

--- resources/views/frontend/homepage/index.blade.php
+++ resources/views/frontend/homepage/index.blade.php
@@ -1,5 +1,22 @@
 @extends('frontend.layouts.app')
 
+@push('meta')
+    <title>Cheap Nusa Penida Lembongan Tour - Lembongan Ocean Adventure</title>
+    <meta name="description" content="Book now! let's go snorkeling and go around nusa lembongan and nusa penida">
+    <meta name="keywords" content="nusa penida, nusa penida tour, blue lagoon lembongan, yellow bridge, nusa lembongan, nusa lembongan tour, dream beach lembongan, snorkeling nusa penida, manta, kelingking, atuh, broken beach, crystal bay">
+
+    <meta property="og:url" content="{{ ('/') }}" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Nusa Penida Lembongan Tour - Lembongan Ocean Adventure" />
+    <meta property="og:description" content="Book now! let's go snorkeling and go around nusa lembongan and nusa penida" />
+    <meta property="og:image" content="{{ asset('frontend/img/penidatrip-thumbnail.jpeg') }}" />
+
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="Nusa Penida Lembongan Tour - Lembongan Ocean Adventure" />
+    <meta name="twitter:description" content="Book now! let's go snorkeling and go around nusa lembongan and nusa penida" />
+    <meta name="twitter:image" content="{{ asset('frontend/img/motorbike_tour.jpg') }}" />
+@endpush
+
 @push('styles')
     <link rel="stylesheet" href="{{ asset('frontend/css/home.css') }}">
 @endpush
