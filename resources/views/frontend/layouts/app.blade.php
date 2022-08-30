723ded00454db9e14177fba45dd10345baa5a169
Bali Merta <made.b4li@gmail.com>
2022-08-30 10:48:36 +0700

    Update app.blade.php

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -6,7 +6,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="google-site-verification" content="o82UidUsptg1_N4x599a9hwYOgAOUI7BzLEsXGANcNo" />
     {{-- <title>Snorkling nusa lembongan and nusa penida trip - Lembongan Ocean Adventure</title> --}}
 
     @stack('meta')
